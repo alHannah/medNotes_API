ef13e85183f880878b818b7769758129dac1851a
hannahsim <111653900+hannah442003@users.noreply.github.com>
2025-06-08 13:49:05 +0800

    rebuilded schema wil continue this project later

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+// instance for prisma/client
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], //use for development for better error logging
+});
+
+// shotdown handling prevent db from hanging
+process.on('beforeExit', async () => {
+    await prisma.$disconnect();
+});
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+});
 
 export default prisma;
---
+++ src/utils/softDeleteCascade.js
@@ -0,0 +1,6 @@
+let a = 6;
+let b = 10;
+
+const add = (a, b) => {
+    return a + b;
+};
